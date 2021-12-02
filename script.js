4d3264aef1fc9abb5a1e53ffba36581ef2638d9f
aru99 <mohammadarmanmavric@gmail.com>
2021-12-02 19:14:09 +0530

    current score and player switch functionality added

--- script.js
+++ script.js
@@ -3,23 +3,31 @@
 //Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+//current score
+const currentScore0El = document.querySelector('#current--0');
+const currentScore1El = document.querySelector('#current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const player0 = document.querySelector('.player--0');
-const player1 = document.querySelector('.player--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-//
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+//Event listener for roll button
 btnRoll.addEventListener('click', function () {
   /*todo:
     1. Generate a random dice roll
     2.display the dice 
-    3. check for rolled 1: if true, switch to ext player 
+    3. keep adding score to current player, check for rolled 1: if true, switch to next player 
     */
 
   //1.Generating a random doce roll
@@ -31,11 +39,16 @@ btnRoll.addEventListener('click', function () {
   console.log(diceNum);
 
   //3. Check for 1 : if true, switch to next player
-  if (diceNum === 1 && player0.classList.contains('player--active')) {
-    player0.classList.remove('player--active');
-    player1.classList.add('player--active');
-  } else if (diceNum === 1 && player1.classList.contains('player--active')) {
-    player0.classList.add('player--active');
-    player1.classList.remove('player--active');
+
+  if (diceNum !== 1) {
+    currentScore += diceNum;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
